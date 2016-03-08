73f0188d17be26fb461ab2dce216b6efe7899d12
superpingu <arnaudbonetti@me.com>
2016-03-09 00:26:42 +0100

    bootstrapped libsmall

---
+++ src/fisharm.c
@@ -0,0 +1,10 @@
+#include <robotdriver/motioncontroller.h>
+#include <robotdriver/speedcontroller.h>
+#include <robotdriver/headingcontroller.h>
+#include <robotdriver/motordriver.h>
+#include <robotdriver/toolboxdriver.h>
+#include "fisharm.h"
+
+int hasFish() {
+    return getSensor(2);
+}
---
+++ src/fisharm.h
@@ -0,0 +1,15 @@
+#ifndef FISHARM_H
+#define FISHARM_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* returns 1 if a fish has been captured, 0 otherwise */
+int hasFish();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
