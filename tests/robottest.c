f583e5814f227478ee38e19b46a9bd128b0dbe65
superpingu <arnaudbonetti@me.com>
2016-03-30 11:55:06 +0200

    updated makefile and added start jack test

--- tests/robottest.c
+++ tests/robottest.c
@@ -8,12 +8,18 @@ static void collisionDetect(int sensor) {
 static void collisionEnd(int sensor) {
     printf("No more obstacle in front of sensor %d\n", sensor);
 }
+static void gameStart() {
+    printf("game start !\n");
+}
+
 int main () {
     initRobot();
 
     onCollisionDetect(collisionDetect);
     onCollisionEnd(collisionEnd);
 
+    onGameStart(gameStart);
+    
     printf("Current config : %d\n", getTableConfig());
 
     while(1);
