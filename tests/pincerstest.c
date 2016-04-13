597484501cce8095f2d04da34c063c8bacd25323
Telecom Robotics <team@telecom-robotics.org>
2016-04-13 23:04:02 +0000

    test pinces plus complet

--- tests/pincerstest.c
+++ tests/pincerstest.c
@@ -1,16 +1,24 @@
 #include <stdlib.h>
 #include <librobot/robot.h>
 #include <librobot/pincers.h>
+#include <robotdriver/motioncontroller.h>
+#include <robotdriver/speedcontroller.h>
 
 int main (int argc, char *argv[]) {
 	initRobot();
 	onTryCapture(testCallback);
-	if(atoi(argv[1]) == 2)
-		tryCapture();
-	if(atoi(argv[1]) == 1)
-		openPincers();
-	if(atoi(argv[1]) == 0)
-		closePincers();
+	if(argc == 2){
+		if(atoi(argv[1]) == 2)
+			tryCapture();
+		if(atoi(argv[1]) == 1)
+			openPincers();
+		if(atoi(argv[1]) == 0)
+			closePincers();
+	}
+	else {
+		setBlockingCallback(tryCapture);
+		queueSpeedChange(0.1, NULL);
+	}
 	while(1);
 	return 0;
 }
