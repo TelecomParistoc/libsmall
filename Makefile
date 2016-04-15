549d38a1209fa742f155e8c7c721c1c58d9b9660
Tanguy <trouffignac@enst.fr>
2016-04-15 15:18:22 +0200

    test pour recalibrer près des poissons

--- Makefile
+++ Makefile
@@ -7,7 +7,7 @@ CFLAGS = -O2 -std=gnu99 -Wall -Werror -fpic
 LDFLAGS= -shared -lrobotdriver
 PREFIX = /usr/local
 VPATH = build/
-TESTS = tests/fishingtest tests/moveToFish tests/robottest tests/AX12position tests/yeux tests/pincerstest
+TESTS = tests/fishingtest tests/moveToFish tests/robottest tests/AX12position tests/yeux tests/pincerstest tests/posFish
 
 vpath %.c src/
 vpath %.h src/
---
+++ tests/posFish.c
@@ -0,0 +1,21 @@
+#include <librobot/robot.h>
+#include <robotdriver/motioncontroller.h>
+#include <robotdriver/speedcontroller.h>
+
+static void recalibrate(){
+	fastSpeedChange(0);
+	enableHeadingControl(1);
+	setRobotDistance(0);
+	queueSpeedChange(-0.15, NULL);
+	queueStop(-30, NULL);
+}
+	
+
+int main(){
+	initRobot();
+	setBlockingCallback(recalibrate);
+	enableHeadingControl(0);
+	queueSpeedChange(0.15, NULL);
+	while(1);
+	return 0;
+}
