ae19b0fefb2e135aa9e6ecfc7ba45c1821ea0ac2
Alkanoor <Alkanoor@github.com>
2016-04-30 19:32:45 +0200

    update, a tester

--- src_main/WaysAndActions.hpp
+++ src_main/WaysAndActions.hpp
@@ -1,3 +1,4 @@
+#include <pathfollower/pathFollower.hpp>
 #include "Action.hpp"
 
 /**Attention : ceci n'est qu'un exemple complètement irréaliste.
@@ -6,6 +7,7 @@
 
 std::vector<std::string> ways = {"move_cubes_go_fish","water2net","net2water","water2net","net2water","water2net","net2rocks","rocks2rocks"};
 std::vector<Action> actions;
+std::pair<double,double> curPos = std::pair<double,double>(40,1000);
 
 void initWaysAndActions()
 {
@@ -13,4 +15,9 @@ void initWaysAndActions()
     actions.push_back(Action(&pecher_start,&pecher_pause,&pecher_continue,&pecher_isFinished));
     actions.push_back(Action(&relacher_start,&relacher_pause,&relacher_continue,&relacher_isFinished));
     //etc ...
+
+    /**A decommenter pour le gros robot**/
+    /**PathFollower::setRadiusPositiveSpeed(180);
+    PathFollower::setRadiusNegativeSpeed(120);
+    **/
 }
