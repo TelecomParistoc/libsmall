89325f9fb3c653b806417525681af4c0cdbb75e0
Tanguy <trouffignac@enst.fr>
2016-04-30 23:13:03 +0200

    delay start

--- src_main/WaysAndActions.hpp
+++ src_main/WaysAndActions.hpp
@@ -8,13 +8,13 @@
  * Chaque robot doit avoir son propre fichier ways_and_actions qui doit être rempli en conséquences
  **/
 
-std::vector<std::string> ways = {"start2water", "water2net", "net2water", "water2net", "net2water", "water2net", "net2water", "water2net", "net2rocks"};
+std::vector<std::string> ways = {"start2water", "water2net", "net2water", "water2net", "net2water", "water2net", "net2water", "water2net", "net2rocks", "rocks2start", "start2rocks", "rocks2start2"};
 std::vector<Action> actions;
 std::pair<double,double> curPos = std::pair<double,double>(41,1003);
 
 void initWaysAndActions()
 {
-	actions.push_back(Action());
+	actions.push_back(Action(&delayStart, NULL, NULL, &fishStop, &fishHasFinished));
 	actions.push_back(Action(&startFishing, NULL, NULL, &fishStop, &fishHasFinished));
 	actions.push_back(Action(&releaseFish, NULL, NULL, &fishStop, &fishHasFinished));
 	actions.push_back(Action(&turn2, NULL, NULL, &fishStop, &fishHasFinished));
@@ -24,4 +24,7 @@ void initWaysAndActions()
 	actions.push_back(Action(&turn2, NULL, NULL, &fishStop, &fishHasFinished));
 	actions.push_back(Action(&releaseFish, NULL, NULL, &fishStop, &fishHasFinished));
 	actions.push_back(Action(&catchShells, NULL, NULL, &pincersStop, &pincersHasFinished));
+	actions.push_back(Action(&openPincers, NULL, NULL, &pincersStop, &pincersHasFinished));
+	actions.push_back(Action(&closePincers, NULL, NULL, &pincersStop, &pincersHasFinished));
+	actions.push_back(Action(&openPincers, NULL, NULL, &pincersStop, &pincersHasFinished));
 }
