654d8d55ee487844d51490498241207150e3f002
Telecom Robotics <telecom.robotics@gmail.com>
2016-05-01 20:27:26 +0200

    toutes les batteries sont vides

--- src_main/test_kill_service.cpp
+++ src_main/test_kill_service.cpp
@@ -1,8 +1,19 @@
 #include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 int main()
 {
-    setuid(0);
-    printf("%d\n",system("ps -Al | grep loop"));
+    int ret = system("ps -Al | grep loop");
+    printf("%d\n",ret);
+
+    if(!ret)
+    {
+        printf("Shutting down service\n");
+        setuid(0);
+    	system("service launchLoop.sh stop");
+    }
+    else
+        printf("No service found\n");
     return 0;
 }
