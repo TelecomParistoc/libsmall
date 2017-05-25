b7ccb5edc3c1a7541efe6e1e992a4d4fee694fdb
superpingu <arnaudbonetti@me.com>
2017-05-25 20:24:05 +0200

    add untracked file

---
+++ src/robot.h
@@ -0,0 +1,15 @@
+#ifndef ROBOT_H
+#define ROBOT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// call actions from input text file
+int readAndCall(FILE * file, char c);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // ROBOT_H
